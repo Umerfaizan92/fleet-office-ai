c55a58c49cde5793e17f2e804e547856e506e157
Umerfaizan92 <umerfaizan@yahoo.com>
2026-09-19 21:50:48 +0800

    Refresh PWA cache for transcription fix

--- saas/sw.js
+++ saas/sw.js
@@ -1,4 +1,4 @@
-const CACHE='superpro-public-20260919-13';
+const CACHE='superpro-public-20260919-14';
 const PUBLIC=[
   '/saas/',
   '/saas/index.html',
@@ -10,7 +10,7 @@ const PUBLIC=[
   '/saas/brand.css?v=20260919.6',
   '/saas/experience.css?v=20260919.6',
   '/saas/product-guide.js?v=23',
-  '/saas/intro.js?v=28',
+  '/saas/intro.js?v=29',
   '/saas/support.js?v=13',
   '/saas/common.js?v=20260919.6',
   '/saas/install.js?v=20260919.6',
